1361ee74ef424313200d095233a47adeabad63ae
Big Fat Coder <33883361+TheFatFatCoder@users.noreply.github.com>
2018-09-23 21:18:12 +0700

    added script to homepage

--- homepage.php
+++ homepage.php
@@ -43,7 +43,9 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
   <link rel="stylesheet" href="./bootstrap-clockpicker/css/bootstrap-material-datetimepicker.css">
-
+  <script src="https://www.gstatic.com/firebasejs/5.5.1/firebase.js"></script>
+  <script src="./functions/firebase/config.js"></script>
+  <script src="./functions/firebase/loadStatus.js"></script>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
